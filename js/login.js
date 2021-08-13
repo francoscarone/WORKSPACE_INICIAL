14bc6864fea5df53fb92c83bc6ef370ecf341805
francoscarone <francoscaronefgsm@gmail.com>
2021-08-13 17:17:58 -0300

    tercera.5 prueba login google

--- js/login.js
+++ js/login.js
@@ -3,11 +3,12 @@
 //elementos HTML presentes.
 document.addEventListener("DOMContentLoaded", function(e){
     
-
+  
 });
+
 function onSignIn(googleUser) {
     // Useful data for your client-side scripts:
-   /* var profile = googleUser.getBasicProfile();
+   /*var profile = googleUser.getBasicProfile();
     console.log("ID: " + profile.getId()); // Don't send this directly to your server!
     console.log('Full Name: ' + profile.getName());
     console.log('Given Name: ' + profile.getGivenName());
@@ -19,17 +20,27 @@ function onSignIn(googleUser) {
     var id_token = googleUser.getAuthResponse().id_token;
     console.log("ID Token: " + id_token);
     location.href="index.html"
+    document.getElementById("log_user").innerHTML=console.log(profile.getName)
   }
 
 function comprobar(){
  
     let campoUser = document.getElementById("user");
     let campoPass = document.getElementById("password");
+    let users = {};
 
     if (campoUser.value.trim()==='' || campoPass.value.trim()===''){
         alert("Debe completar los campos");
     }else{
        location.href="index.html"
+
+        users.nombre = campoUser.value;
+        users.estado ="conectado";
+//---------->
+        localStorage.setItem('usuario',JSON.stringify(users)); //Guardo mi variable de objeto en Local Storage
+        
+        sessionStorage.setItem('usuario',JSON.stringify(users));
     }
 
 }
+
