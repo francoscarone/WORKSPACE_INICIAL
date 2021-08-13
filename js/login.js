7c9bc0049c8ea5f0fb073b7bf21297530cd654a4
francoscarone <francoscaronefgsm@gmail.com>
2021-08-13 16:36:51 -0300

    tercera.3 prueba login google

--- js/login.js
+++ js/login.js
@@ -18,7 +18,7 @@ function onSignIn(googleUser) {
     // The ID token you need to pass to your backend:
     var id_token = googleUser.getAuthResponse().id_token;
     console.log("ID Token: " + id_token);
-    location.href="https://francoscarone.github.io/WORKSPACE_INICIAL"
+    location.href="index.html"
   }
 
 function comprobar(){
