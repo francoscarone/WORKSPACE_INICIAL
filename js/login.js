f45bba985e71a47c697071452e06e7bef047c734
francoscarone <francoscaronefgsm@gmail.com>
2021-08-17 22:17:45 -0300

    cambiando index

--- js/login.js
+++ js/login.js
@@ -1,12 +1,5 @@
-//Función que se ejecuta una vez que se haya lanzado el evento de
-//que el documento se encuentra cargado, es decir, se encuentran todos los
-//elementos HTML presentes.
-document.addEventListener("DOMContentLoaded", function(e){
-    
-  
-});
 
-function onSignIn(googleUser) {
+/*function onSignIn(googleUser) {
     // Useful data for your client-side scripts:
    var profile = googleUser.getBasicProfile();
     console.log("ID: " + profile.getId()); // Don't send this directly to your server!
@@ -20,38 +13,40 @@ function onSignIn(googleUser) {
     var id_token = googleUser.getAuthResponse().id_token;
     console.log("ID Token: " + id_token);
     
-  }
+}
+*/
 
 function comprobar(){
  
     let campoUser = document.getElementById("user");
     let campoPass = document.getElementById("password");
-    let users = {};
+    let usuario = {};
 
     if (campoUser.value.trim()==='' || campoPass.value.trim()===''){
         alert("Debe completar los campos");
     }else{
-       location.href="index.html"
-
-        users.nombre = campoUser.value;
-        users.estado ="conectado";
+        location.href="inicio.html"
+        usuario.nombre = campoUser.value;
+        usuario.estado ="conectado";
 //---------->
-        localStorage.setItem('usuario',JSON.stringify(users)); //Guardo mi variable de objeto en Local Storage
-        
-        sessionStorage.setItem('usuario',JSON.stringify(users));
+        localStorage.setItem('usuario',JSON.stringify(usuario)); //Guardo mi variable de objeto en Local Storage
+        sessionStorage.setItem('usuario',JSON.stringify(usuario));
     }
 
 }
 
-function signOut(){
-    
-    var auth2 = gapi.auth2.getAuthInstance();
-    auth2.signOut().then(function () {
-      console.log('User signed out.');
-    });
-    localStorage.clear(); ///Borra toooodo el localStorage
-    location.href="index.html"; //De donde esté, no importa dónde, me envía a index.html
 
 
-}
+//Función que se ejecuta una vez que se haya lanzado el evento de
+//que el documento se encuentra cargado, es decir, se encuentran todos los
+//elementos HTML presentes.
+document.addEventListener('DOMContentLoaded', ()=>{
+    let usuario = JSON.parse(localStorage.getItem("usuario"));
+    if (usuario.estado=='conectado'){
+        //location.href="inicio.html";
+    } else{
+        location.href="login.html";
+    }
+
+});
 
--- js/products.js
+++ js/products.js
@@ -1,6 +1,51 @@
+var productosArray = [];
+
+function showProductList(array){
+
+
+    let productos = "";
+    for(let i = 0; i < array.length; i++){
+        let producto = array[i];
+        
+        productos += `
+        <div class="list-group-item list-group-item-action">
+            <div class="row">
+                <div class="col-3">
+                    <img src="${producto.imgSrc}" alt=" ${producto.description}" class="img-thumbnail">
+                </div>
+                <div class="col">
+                    <div class="d-flex w-100 justify-content-between">
+                        <h4 class="mb-1">${producto.name}</h4>
+                        <small class="text-muted"> ${producto.soldCount} artículos</small>
+                    </div>
+                    <p class="mb-1">${producto.description}</p>
+                    <br>
+                    <br>
+                    <div><p style="float:right;""class="mb-1">Precio: ${producto.currency} ${producto.cost} </p></div> 
+
+                </div>
+            </div>
+        </div>
+        `
+
+        document.getElementById("product-list-contain").innerHTML = productos;
+
+        hideSpinner();
+    }   
+}
+
+
 //Función que se ejecuta una vez que se haya lanzado el evento de
 //que el documento se encuentra cargado, es decir, se encuentran todos los
 //elementos HTML presentes.
-document.addEventListener("DOMContentLoaded", function (e) {
-
+document.addEventListener("DOMContentLoaded", function(e){
+    showSpinner();
+    getJSONData(PRODUCTS_URL).then(function(resultObj){
+        if (resultObj.status === "ok")
+        {
+            productosArray = resultObj.data;
+            //Muestro las categorías ordenadas
+            showProductList(productosArray);
+        }
+    });
 });
