3610ee86250f22f5d4653901fe414dc7378015ee
francoscarone <francoscaronefgsm@gmail.com>
2021-10-15 13:42:39 -0300

    Entrega 5 casi finalizada

--- js/cart.js
+++ js/cart.js
@@ -1,43 +1,98 @@
 carrito = {};
+let subTotal_Final=0;
 function showCarrito(lista){
     let tabla = ``;
     for(let i = 0; i < lista.articles.length; i++){
         let articulo = lista.articles[i];
-        if(articulo.currency=="USD"){
-            articulo.currency="UYU";
-            articulo.unitCost=articulo.unitCost*40;
+        if(articulo.currency=="UYU"){
+            articulo.currency="USD";
+            articulo.unitCost=articulo.unitCost/40;
         }
         tabla +=`
-        <div class="table-responsive-xl">
-            <table class="table">
-                <thead class="thead-dark">    
-                    <tr>
-                        <th class"col">${articulo.name}</th>
-                        <th class"col">Cantidad</th>
-                        <th class"col">Costo Unitario</th>
-                        <th class"col">Sub Total</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    <tr>
-                        <td><img src="${articulo.src}" class="rounded mx-auto d-block" style="width:30%"></td>
-                        <td><input id="cantidad${i}"type="number" min=1 max=10 value=1 onchange="calSubtotal(${i},${articulo.unitCost});"></td>
-                        <td>${articulo.currency} ${articulo.unitCost}</td>
-                        <td id="subtotal${i}"></td>
-                    </tr>
-                </tbody>
-            </table>
-        </div>
+
+        <div
+              class="
+                d-flex
+                justify-content-between
+                align-items-center
+                mt-3
+                p-2
+                items
+                rounded
+              "
+            >
+              <div class="d-flex flex-row">
+                <img
+                  class="rounded"
+                  src="${articulo.src}"
+                  width="120"
+                  height="105"
+                />
+                <div class="d-flex flex-row align-items-center">
+                <div class="ml-2">
+                  <span class="font-weight-bold d-block">${articulo.name}</span>
+                </div>
+            </div>
+              </div>
+              <div class="d-flex flex-row align-items-center">
+                <div class="ml-2">
+                    <span class="font-weight-bold d-block">Cantidad</span>
+                    <span class="d-block font-weight-bold"><input id="cantidad${i}"type="number" min=0 max=10 value=0 onchange="calSubtotal(${i},${articulo.unitCost});"></span>
+                  </div>
+                  <div class="ml-2">
+                    <span class="font-weight-bold d-block">Precio Unitario</span>
+                    <span class="d-block font-weight-bold">${articulo.currency}&nbsp;${articulo.unitCost}</span>
+                  </div>
+                  <div class="ml-2">
+                    <span class="font-weight-bold d-block">Sub total</span>
+                    <div class="d-flex justify-content-between information">
+                      <span>USD </span><span class="d-block font-weight-bold" id="subtotal${i}">${0}</span>
+                    </div>
+                   </div>
+              </div>
+            </div>
     `
-    document.getElementById("articulos-container").innerHTML = tabla;
+    document.getElementById("articles-cart").innerHTML = tabla;
+    document.getElementById("cant-prod-cart").innerText = "Hay "+lista.articles.length+" articulos en su carrito";
     }
     
 }
 
-function calSubtotal(indice,precio){
+function calSubtotal(indice,precio){   
+
+  // cAMBIAR EL VALOR DEL CARRITO AL SUMAR O RESTAR UNIDADES
     cantidad=document.getElementById("cantidad"+indice).value;
+    
     subtotal = precio * cantidad;
-    document.getElementById("subtotal"+indice).innerHTML=subtotal;
+    document.getElementById("subtotal"+indice).innerHTML= subtotal;
+
+    calcCostoEnvio()
+   
+}
+
+function calcCostoEnvio(){
+  let total_Final=0;
+  let subTotal_Final=0;
+  let costoEnvio=0
+  for (let index = 0; index < carrito.articles.length; index++) {
+    subTotal_Final += parseFloat(document.getElementById("subtotal"+index).innerHTML)  
+  }
+  document.getElementById("subTotal-final").innerText ="USD "+ subTotal_Final.toFixed(2)
+
+  let envio = document.getElementsByName("card");
+    if(envio[0].checked){
+      costoEnvio=(subTotal_Final*0.15).toFixed(2)
+      document.getElementById("costo-envio").innerText="USD "+ costoEnvio
+    }
+    if(envio[1].checked){
+      costoEnvio = (subTotal_Final*0.07).toFixed(2)
+      document.getElementById("costo-envio").innerText="USD "+ costoEnvio
+    }if(envio[2].checked){
+      costoEnvio = (subTotal_Final*0.05).toFixed(2)
+      document.getElementById("costo-envio").innerText="USD "+ costoEnvio
+    }
+    total_Final = (parseFloat(subTotal_Final)+parseFloat(costoEnvio)).toFixed(2);
+    document.getElementById("total-cost").innerText="USD "+ total_Final;
 }
 
 
