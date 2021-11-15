effc0e9005cf023e11d8308d41705ea62f982287
francoscarone <francoscaronefgsm@gmail.com>
2021-11-14 22:08:26 -0300

    Enrega 7 Finalizada

--- js/cart.js
+++ js/cart.js
@@ -1,5 +1,5 @@
 carrito = {};
-
+camposDePago=[];
 function showCarrito(lista){
     let tabla = ``;
     for(let i = 0; i < lista.articles.length; i++){
@@ -49,7 +49,7 @@ function showCarrito(lista){
                   <div class="ml-2">
                     <span class="font-weight-bold d-block">Sub total</span>
                     <div class="d-flex justify-content-between information">
-                      <span>USD </span><span class="d-block font-weight-bold" id="subtotal${i}">${articulo.count * articulo.unitCost}</span>
+                      <span>USD </span><span class="d-block font-weight-bold" id="subtotal${i}">${articulo.count * articulo.unitCost}</span><i onclick="eliminar(${i});" class="fas fa-trash-alt delete"></i>
                     </div>
                    </div>
                 
@@ -61,6 +61,19 @@ function showCarrito(lista){
     }
     
 }
+function eliminar(i){
+
+carrito.articles.splice(i, 1);
+if (carrito.articles.length>=1){
+  showCarrito(carrito);
+  calcCostoEnvio()
+}else{
+  document.getElementById("articles-cart").innerHTML = `<h3>No hay articulos en su carrito</h3>`
+  document.getElementById("cant-prod-cart").innerText = "Hay 0 articulos en su carrito";
+  calcCostoEnvio()
+}
+}
+
 
 function calSubtotal(indice,precio){   
 
@@ -110,14 +123,107 @@ function calcCostoEnvio(){
     document.getElementById("total-cost").innerText="USD "+ total_Final;
 }
 
+// comprobamos que todos los campos esten completos
+// si estan completos, mostramos una animacion
 function comprar(){
-  Swal.fire({
-    position: 'middle',
-    html: '<lottie-player src="https://assets2.lottiefiles.com/packages/lf20_4eth4jy9.json"  background="transparent"  speed="1"  style="width: 450px; height: 450px;"    autoplay></lottie-player>',
-    title: 'Compra realizada, Gracias!',
-    showConfirmButton: false,
-    timer: 3000
-  })
+  let radios = document.getElementsByName("formaPago")
+  let campos = document.querySelectorAll('input[type=text]');
+  let contador = 0;
+  guardarFormaPago()
+
+  if(radios[0].checked==false && radios[1].checked==false){
+    alert("Seleccione una forma de pago")
+    contador+=1;
+  }
+  if(contador>0){
+  alert("Complete los campos de forma de pago")
+  }
+  for (let campo of campos) {
+    
+    if(campo.value.trim()===''){
+      contador+=1;
+      campo.className="form-control is-invalid";
+      campo.placeholder="Debe completar este campo";
+    }else{
+      campo.className="form-control is-valid"
+    }
+  }
+  if (contador===0){
+    Swal.fire({
+      position: 'middle',
+      html: '<lottie-player src="https://assets2.lottiefiles.com/packages/lf20_4eth4jy9.json"  background="transparent"  speed="1"  style="width: 450px; height: 450px;"    autoplay></lottie-player>',
+      title: 'Compra realizada, Gracias!',
+      showConfirmButton: false,
+      timer: 3000
+    })
+  }
+    
+} 
+
+// generamos la ventana modal para cada fomra de pago
+function formaPago(){
+// habilito y deshabilito campos en funcion del pago seleccionado
+  let radios = document.getElementsByName("formaPago")
+  if(radios[0].checked){
+    document.getElementById("numCuenta").disabled=true
+    document.getElementById("nombApell2").disabled=true
+    document.getElementById("numTarjeta").value=""
+    document.getElementById("nombApell").value=""
+    document.getElementById("fechaExp").value=""
+    document.getElementById("cedula").value=""
+    document.getElementById("codSeg").value=""
+    document.getElementById("numTarjeta").disabled=false
+    document.getElementById("nombApell").disabled=false
+    document.getElementById("fechaExp").disabled=false
+    document.getElementById("cedula").disabled=false
+    document.getElementById("codSeg").disabled=false
+    document.getElementById("numCuenta").value="deshabilitado"
+    document.getElementById("nombApell2").value="deshabilitado"
+   
+  }
+  if(radios[1].checked){
+    document.getElementById("numCuenta").value=""
+    document.getElementById("nombApell2").value=""
+    document.getElementById("numTarjeta").disabled=true
+    document.getElementById("nombApell").disabled=true
+    document.getElementById("fechaExp").disabled=true
+    document.getElementById("codSeg").disabled=true
+    document.getElementById("cedula").disabled=true
+    document.getElementById("numTarjeta").value="deshabilitado"
+    document.getElementById("nombApell").value="deshabilitado"
+    document.getElementById("fechaExp").value='0001-01-01'
+    document.getElementById("codSeg").value="deshabilitado"
+    document.getElementById("cedula").value="deshabilitado"
+    document.getElementById("numCuenta").disabled=false
+    document.getElementById("nombApell2").disabled=false
+  }
+}
+
+// validamos la forma de pago
+
+function guardarFormaPago(){
+ 
+
+  // comprobamos si hay campos vacios, si los hay, 
+  // aumentamos el contador y resaltamos los campos que deban rellenarse
+  camposDePago = document.getElementsByName("camposPago");
+  let contador=0;
+  for(i=0 ; i < camposDePago.length ; i++){
+  if(camposDePago[i].value.trim()===''){
+    camposDePago[i].className="form-control is-invalid";
+    camposDePago[i].placeholder="Debe completar este campo";
+    contador+=1;
+  }else{
+    camposDePago[i].className="form-control is-valid"
+  }
+  }  
+
+  // condicion para cerrar el modal o lanzar alerta de que hay campos vacios
+  if(contador===0){
+    $('#formaPagoModal').modal("hide");
+  }else{
+    alert("Debe rellenar los campos de forma de pago")
+  }
 }
 
 
